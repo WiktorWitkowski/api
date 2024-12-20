69b6f65d08547a5d54f4ff25822129936353d766
Wiktor-Witkowski_sgo <wwitkowski.dev@gmail.com>
2024-12-20 09:39:59 +0100

    add check helth

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/check_health")
+def check_health():
+    return {"API": "Up and Running!"}
